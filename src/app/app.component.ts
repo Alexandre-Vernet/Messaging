9cb49ac86d97b144c91f421db8d86a0b8247c7d2
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-06-02 20:59:29 +0200

    Remove left pannel Add array with message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,30 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { MessagesService } from './Services/messages/messages.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  messages: any;
+  // user: String;
+
+  constructor(private messagesService: MessagesService) {}
+
+  ngOnInit() {
+    this.messages = this.messagesService.messages;
+
+    // this.user = 'Alex';
+  }
+
+  formatDate = (message: Date): string => {
+    let day = message.getDate();
+    let month = message.getMonth();
+    let hours = message.getHours();
+    let minutes = message.getMinutes();
+
+    return day + ' / ' + month + ' at ' + hours + ' : ' + minutes;
+  };
+}
