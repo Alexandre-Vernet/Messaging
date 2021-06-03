bec661d0839759decc2f94a2af48fba317dec888
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-06-03 21:16:43 +0200

    Add / Store message in Firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,75 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { MessagesService } from './Services/messages/messages.service';
-
+import firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  messages: any;
-  // user: String;
+  message: any;
+  messages: { user: String; message: String }[] = [];
 
-  constructor(private messagesService: MessagesService) {}
+  firestore: any;
+
+  constructor() {
+    // Firestore
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAhod5EQ_wRW3eEz8Zsaw3Ya6WCQ9sldlg',
+      authDomain: 'messaging-db163.firebaseapp.com',
+      projectId: 'messaging-db163',
+      storageBucket: 'messaging-db163.appspot.com',
+      messagingSenderId: '901664692612',
+      appId: '1:901664692612:web:d97bc043a1b2f99f5ee5d1',
+      measurementId: 'G-LXVMRHP2J0',
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
 
   ngOnInit() {
-    this.messages = this.messagesService.messages;
+    this.firestore = firebase.firestore();
 
-    // this.user = 'Alex';
+    this.getMessages();
   }
+
+  getMessages = () => {
+    this.firestore
+      .collection('messages')
+      .get()
+      .then((querySnapshot: any) => {
+        querySnapshot.forEach((doc: any) => {
+          let message: String = doc.data();
+
+          this.messages.push({
+            user: doc.get('user'),
+            message: doc.get('message'),
+            // date: doc.get('date'),
+          });
+        });
+      });
+  };
+
+  sendMessage = () => {
+    // Store message
+    this.firestore
+      .collection('messages')
+      .add({
+        user: 'Toto',
+        message: this.message,
+        date: new Date(),
+      })
+      .then((docRef: { id: string }) => {
+        console.log('Message successfull posted with id' + docRef.id);
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+
+    // Clear input
+    this.message = '';
+  };
 
   formatDate = (message: Date): string => {
     let day = message.getDate();
