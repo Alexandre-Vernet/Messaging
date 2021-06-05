b9dfd9811e191cca7d31ae4f9ee68989aec5cbb4
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-06-05 21:40:35 +0200

    Add sign-up with Firebase

--- src/app/Components/login/sign-up/sign-up.component.ts
+++ src/app/Components/login/sign-up/sign-up.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import firebase from 'firebase';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  signUp(form: NgForm) {
+    const email = form.value['email'];
+    const password = form.value['password'];
+
+    console.log(email, password);
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   }
-
 }
