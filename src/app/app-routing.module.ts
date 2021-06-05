4edf7bb342651dc328546114acdaec46635fa0f6
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-06-05 18:49:48 +0200

    Add form to sign-in / sign-up / forgot-password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
-import { SignInComponent } from './Components/sign-in/sign-in.component';
-import { SignUpComponent } from './Components/sign-up/sign-up.component';
+import { SignInComponent } from './Components/login/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/login/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './Components/login/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent },
-  { path: '**', component: AppComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
-import { SignInComponent } from './Components/sign-in/sign-in.component';
-import { SignUpComponent } from './Components/sign-up/sign-up.component';
-
+import { SignInComponent } from './Components/login/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/login/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './Components/login/forgot-password/forgot-password.component';
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SignInComponent, SignUpComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
