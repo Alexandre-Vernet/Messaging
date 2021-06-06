62d6a6ad6a146cb454f423eba0eec6e50f644a58
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-06-06 11:17:47 +0200

    Add rules validation form sign-up

--- src/app/Components/login/sign-up/sign-up.component.ts
+++ src/app/Components/login/sign-up/sign-up.component.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import {
+  Form,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import firebase from 'firebase';
+
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
-  constructor() {}
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {}
 
-  signUp(form: NgForm) {
-    const email = form.value['email'];
-    const password = form.value['password'];
+  signUp() {
+    /**
+     * The email address is already in use by another account.
+     * Password should be at least 6 characters
+     * The email address is badly formatted.
+     */
 
-    console.log(email, password);
+    console.log(this.form.value);
 
+    const email = this.form.value['email'];
+    const password = this.form.value['password'];
+    const confirmPassword = this.form.value['confirmPassword'];
+    if (password !== confirmPassword) {
+    }
     firebase
       .auth()
       .createUserWithEmailAndPassword(email, password)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
@@ -15,7 +15,7 @@ import { ForgotPasswordComponent } from './Components/login/forgot-password/forg
     SignUpComponent,
     ForgotPasswordComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
