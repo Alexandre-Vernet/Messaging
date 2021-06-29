3b2f051435dbbdb313f7efccba86a216e3c138b6
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-06-29 17:57:49 +0200

    export config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import firebase from 'firebase';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthenticationService } from './services/authentication/authentication.service';
-
+import * as config from '../config';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -18,18 +18,8 @@ export class AppComponent implements OnInit {
         private cookieService: CookieService,
         private auth: AuthenticationService
     ) {
-        // Firebase
-        const firebaseConfig = {
-            apiKey: 'AIzaSyAhod5EQ_wRW3eEz8Zsaw3Ya6WCQ9sldlg',
-            authDomain: 'messaging-db163.firebaseapp.com',
-            projectId: 'messaging-db163',
-            storageBucket: 'messaging-db163.appspot.com',
-            messagingSenderId: '901664692612',
-            appId: '1:901664692612:web:d97bc043a1b2f99f5ee5d1',
-            measurementId: 'G-LXVMRHP2J0',
-        };
-
-        firebase.initializeApp(firebaseConfig);
+        // Implement firebase
+        firebase.initializeApp(config.firebaseConfig);
     }
 
     ngOnInit() {
