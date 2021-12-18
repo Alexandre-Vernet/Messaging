3aae24eda13957807f209a1fd8b4bbd5464b1858
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2021-12-18 13:42:19 +0100

    Move config firebase to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,25 @@
+import { initializeApp } from 'firebase/app';
+
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+    production: false
 };
+
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyAhod5EQ_wRW3eEz8Zsaw3Ya6WCQ9sldlg',
+    authDomain: 'messaging-db163.firebaseapp.com',
+    projectId: 'messaging-db163',
+    storageBucket: 'messaging-db163.appspot.com',
+    messagingSenderId: '901664692612',
+    appId: '1:901664692612:web:d97bc043a1b2f99f5ee5d1',
+    measurementId: 'G-LXVMRHP2J0',
+};
+
+initializeApp(firebaseConfig);
 
 /*
  * For easier debugging in development mode, you can import the following file
