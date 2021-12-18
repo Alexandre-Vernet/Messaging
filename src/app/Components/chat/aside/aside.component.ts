5a79905f062527ff3519969811cf416e87062908
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2021-12-18 13:46:42 +0100

    Fix TSLint

--- src/app/Components/chat/aside/aside.component.ts
+++ src/app/Components/chat/aside/aside.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-aside',
-  templateUrl: './aside.component.html',
-  styleUrls: ['./aside.component.scss']
+    selector: 'app-aside',
+    templateUrl: './aside.component.html',
+    styleUrls: ['./aside.component.scss']
 })
-export class AsideComponent implements OnInit {
+export class AsideComponent {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit(): void {
-  }
 
 }
