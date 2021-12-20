1357efc6df935e0938c5121dc06eaa27c84b1d17
Alexandre Vernet <alexandre.vernet@g-mail.fr>
2021-12-20 10:46:56 +0100

    Create HeadComponent

--- src/app/Components/home/home.module.ts
+++ src/app/Components/home/home.module.ts
@@ -6,10 +6,11 @@ import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProfileComponent } from '../chat/profile/profile.component';
 import { ContactsComponent } from '../chat/contacts/contacts.component';
+import { HeadComponent } from '../chat/head/head.component';
 
 
 @NgModule({
-    declarations: [HomeComponent, ProfileComponent, ContactsComponent],
+    declarations: [HomeComponent, ProfileComponent, ContactsComponent, HeadComponent],
     imports: [
         CommonModule,
         HomeRoutingModule,
